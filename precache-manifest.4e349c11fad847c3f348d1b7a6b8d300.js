3ab9b6ea7588bd2cf29f30148c4c92c31f6d06ec
Saurav Vaidya <svaidya.se@gmail.com>
2022-07-08 21:25:20 +0530

    Updates

---
+++ precache-manifest.4e349c11fad847c3f348d1b7a6b8d300.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "0c50de47be9a934e839d",
+    "url": "/To-Do-SimpleList/static/css/main.9c1441a6.chunk.css"
+  },
+  {
+    "revision": "0c50de47be9a934e839d",
+    "url": "/To-Do-SimpleList/static/js/main.0c50de47.chunk.js"
+  },
+  {
+    "revision": "cc360fb6938c4a946641",
+    "url": "/To-Do-SimpleList/static/js/1.cc360fb6.chunk.js"
+  },
+  {
+    "revision": "cb1335819c6f651c03ba",
+    "url": "/To-Do-SimpleList/static/js/2.cb133581.chunk.js"
+  },
+  {
+    "revision": "de0f7a0a6477212f4e0b",
+    "url": "/To-Do-SimpleList/static/js/runtime~main.de0f7a0a.js"
+  },
+  {
+    "revision": "a48f00be214427105ebc74f66bdcf405",
+    "url": "/To-Do-SimpleList/index.html"
+  }
+];
